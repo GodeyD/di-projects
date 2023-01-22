9c89f0f6f9567df09ab0ea6c5499b8d8bdb9218e
diego ripari <diego_ripari@hotmail.com>
2023-01-22 11:33:55 +0200

    crenamedfolderandjs

---
+++ Week2/JsGreen/script.js
@@ -0,0 +1,16 @@
+let addressNumber = 3
+let addressStreet = "ye´elim"
+let country = "Israel"
+
+let globalAddress = `i live in ${addressNumber} ${addressStreet}, in ${country}`
+
+console.log(globalAddress)
+
+let birthYear = 1986
+let futureYear = 2030
+
+let actualYear = futureYear-birthYear
+
+console.log(actualYear)
+
+console.log(`I will be ${actualYear} in ${futureYear}`)

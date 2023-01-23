a06e2bb6f4300d65c347f3e6b30f7641f9c03217
diego ripari <diego_ripari@hotmail.com>
2023-01-23 11:50:05 +0200

    dailycomplete

---
+++ Week2/Day2/DailyChallenge/script.js
@@ -0,0 +1,6 @@
+//Daily Challenge: Not Bad
+let sentence = "The movie is not that bad, I like it"
+let wordNot = sentence.indexOf(`not`)
+let wordBad = sentence.indexOf(`bad`)
+
+if (wordBad > wordNot) {console.log(sentence.replace(`${sentence.slice(wordNot, wordBad+3)}`, "good"))}

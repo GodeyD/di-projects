3612cb704f976af4fee012159c78e0b9679bc8d0
diego ripari <diego_ripari@hotmail.com>
2023-01-27 21:42:35 +0200

    add1star

--- Week2/Day3/DailyChallenge/script.js
+++ Week2/Day3/DailyChallenge/script.js
@@ -1,14 +1,14 @@
 // first
 let stars = ""
 
-for (let i=0; i<5; i++) {
+for (let i=0; i<6; i++) {
     stars += "*"
     console.log(stars)
 }
 
 // second
 let starsS = ""
-for (let i=0; i<5; i++) {
+for (let i=0; i<6; i++) {
     for (let j=0; j<1; j++)
     starsS += "*"
     console.log(starsS)

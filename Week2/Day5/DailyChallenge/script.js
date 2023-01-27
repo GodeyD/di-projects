400d5eda87aee3c145ccda45d99306b66df2d06a
diego ripari <diego_ripari@hotmail.com>
2023-01-27 21:26:16 +0200

    dailycomplete

---
+++ Week2/Day5/DailyChallenge/script.js
@@ -0,0 +1,30 @@
+
+let bottleInWall = 99
+let bottleInTheBoys = 0
+
+function song(bottleInWall, bottleInTheBoys) {
+    while (bottleInTheBoys==0) {
+        bottleInTheBoys+=1
+        bottleInWall-=1
+    console.log(`99 bottles of beer on the wall`)
+    console.log(`99 bottles of beer`)
+    console.log(`Take 1 down, pass it around`)
+    console.log(`98 bottles of beer on the wall`)   
+    console.log(" ") 
+    }
+    while (bottleInTheBoys>0 && bottleInWall>=0) {       
+    console.log(`${bottleInWall} bottles of beer on the wall`)
+    console.log(`${bottleInWall} bottles of beer`)
+    bottleInTheBoys+=1
+    bottleInWall-=bottleInTheBoys
+    console.log(`Take ${bottleInTheBoys} down, pass them around`)
+    if (bottleInWall<0) {
+        console.log("no bottle of beer on the wall")
+    } else {
+    console.log(`${bottleInWall} bottles of beer on the wall`)
+    console.log(" ") 
+    }   
+}
+}
+
+song(bottleInWall, bottleInTheBoys)

d7fdf7913d79831ac0492394ac2fe5d839e1affe
diego ripari <diego_ripari@hotmail.com>
2023-02-10 20:23:10 +0200

    xpcomplete

---
+++ Week4/Day1/ExercisesXP/exp.py
@@ -0,0 +1,45 @@
+# Exercise 1 : Hello World
+print('Hello World\n' * 4)
+
+# Exercise 2 : Some Math
+math = (99^3)*8
+print(math)
+
+# Exercise 3 : What Is The Output ?
+
+# >>> 5 < 3 -> false
+# >>> 3 == 3 -> true
+# >>> 3 == "3" -> true
+# >>> "3" > 3 -> false
+# >>> "Hello" == "hello" -> false
+
+#  Exercise 4 : Your Computer Brand
+computer_brand = 'Asus'
+print(f"I have a {computer_brand} computer")
+
+# Exercise 5 : Your Information
+name = 'Diego'
+age = 36
+shoe_size = 42
+info = f'I am not anything, i am {name} i have {age} i step in {shoe_size} be aware'
+print(info)
+
+# Exercise 6 : A & B
+a = 9
+b = 6
+if a > b: print('hello world')
+
+# Exercise 7 : Odd Or Even
+user_number = int (input(''))
+if (user_number % 2) != 0: print('is odd') 
+else: print('is even')
+
+# Exercise 8 : What’s Your Name ?
+whats_your_name = input('tell me your name ')
+if whats_your_name == name: print('How this is possible my tocayo')
+else: print(f'Your name is {whats_your_name}')
+
+# Exercise 9 : Tall Enough To Ride A Roller Coaster
+your_height_in_iches = int (input('Put your iches tall:'))
+if your_height_in_iches > 59 : print('your state are tall enough to ride.')
+else: print('you need to grow some more to ride.')

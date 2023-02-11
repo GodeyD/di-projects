437e76f0095fd9925dffdbf26f56a015aa89b2f3
diego ripari <diego_ripari@hotmail.com>
2023-02-12 00:25:09 +0200

    daylicomplete

--- Week4/Day2/DailyChallenge/dayli.py
+++ Week4/Day2/DailyChallenge/dayli.py
@@ -1,20 +1,108 @@
 # Challenge 1
+# "dodo" ➞ { "d": [0, 2], "o": [1, 3] }
+word = input('Give me a good word: ')
 
-number = int(input('a number plz: '))
-length = int(input('a length plz: '))
-list_numbers = []
+index = []
+letter_key = []
 
-for n in range(length):
-    list_numbers.append((n+1) * number)
-print(f'number: {number} - length {length} ➞ {list_numbers}')
+
+def indices(lst, item):
+   return [i for i, x in enumerate(lst) if x == item]
+
+for letter in word:
+    letter_key.append(letter)    
+    new = indices(word, letter)
+    index.append(new)
+    
+new_dict = dict(zip(letter_key, index))
+
+print(new_dict)
 
 # Challenge 2
-in_str: str = "bbbbbooommmbbbbbooonnn"
+items_purchase = {
+  "Water": "$1",
+  "Bread": "$3",
+  "TV": "$1,000",
+  "Fertilizer": "$20"
+}
 
-result_str = in_str[0]
+wallet = "$300"
 
-for char in in_str[1:]:
-    if result_str[-1] != char:
-        result_str += char
+for key in items_purchase:
+    item: str = items_purchase[key]
+    item_list: list = list(item)
+    del item_list[0]
+    item: str = "".join(item_list)
+    item: str =  item.replace(',','')
+    item: int = int(item)
+    items_purchase[key] = item
 
-print(result_str)  
+
+wallet: int = int(wallet[1:])
+
+items_afford = []
+
+for x in items_purchase.values():   
+    if x < wallet: 
+        items_afford.append(list(items_purchase.keys())[list(items_purchase.values()).index(x)])
+print(items_afford)
+
+items_purchase = {
+  "Apple": "$4",
+  "Honey": "$3",
+  "Fan": "$14",
+  "Bananas": "$4",
+  "Pan": "$100",
+  "Spoon": "$2"
+}
+
+wallet = "$100" 
+
+for key in items_purchase:
+    item: str = items_purchase[key]
+    item_list: list = list(item)
+    del item_list[0]
+    item: str = "".join(item_list)
+    item: str =  item.replace(',','')
+    item: int = int(item)
+    items_purchase[key] = item
+
+
+wallet: int = int(wallet[1:])
+
+items_afford = []
+
+for x in items_purchase.values():   
+    if x <= wallet: 
+        items_afford.append(list(items_purchase.keys())[list(items_purchase.values()).index(x)])
+print(items_afford)
+
+items_purchase = {
+  "Phone": "$999",
+  "Speakers": "$300",
+  "Laptop": "$5,000",
+  "PC": "$1200"
+}
+
+wallet = "$1" 
+
+for key in items_purchase:
+    item: str = items_purchase[key]
+    item_list: list = list(item)
+    del item_list[0]
+    item: str = "".join(item_list)
+    item: str =  item.replace(',','')
+    item: int = int(item)
+    items_purchase[key] = item
+
+
+wallet: int = int(wallet[1:])
+
+items_afford = []
+
+for x in items_purchase.values():   
+    if x <= wallet: 
+        items_afford.append(list(items_purchase.keys())[list(items_purchase.values()).index(x)])
+
+if items_afford == []: print('Nothing')
+else : print(items_afford)

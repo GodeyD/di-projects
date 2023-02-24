7d3e648b8caa89b01f930f9e3cf9b804fe9277cb
diego ripari <diego_ripari@hotmail.com>
2023-02-24 17:41:08 +0200

    dailycomplete

---
+++ Week5/Day3/DailyChallenge/daily.py
@@ -0,0 +1,40 @@
+import math
+
+class Circle:
+
+    def __init__(self, radius: int) -> None:
+        self.radius = radius # 4
+        self.diameter = radius * 2 # 8
+
+    @classmethod
+    def from_diameter(cls, diameter: int):
+        return Circle(diameter//2) 
+    def __str__(self):
+        return f'Radius: {self.radius}, Diameter: {self.diameter}, Area: {self.area():.3f}'
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def __add__(self, other_circle):
+        result = Circle(self.radius + other_circle.radius)
+        return result
+ 
+    def __gt__(self, other_circle) -> bool: 
+        return self.area() > other_circle.area()
+
+    def __eq__(self, other_circle) -> bool:
+        return self.area() == other_circle.area()
+
+circle1 = Circle(2)
+
+print(circle1)
+
+circle2 = Circle.from_diameter(8)
+circle3 = circle1 + circle2
+print(circle3)
+print(circle1 > circle2)
+print(circle1 == circle2)
+circles = [circle3, circle1, circle2]
+circles.sort()
+
+

0b3bed4dc922526eb56766deeb51d3367359307e
diego ripari <diego_ripari@hotmail.com>
2023-03-02 00:56:44 +0200

    mini-proyect-1complete

--- Week5/Day5/ExercisesXP/game.py
+++ Week5/Day5/ExercisesXP/game.py
@@ -1,7 +1,8 @@
 import random
 
 class Game():
-    
+
+    scores = {'win': 0, 'loss': 0, 'draw': 0}
     def get_user_item(self):
         i_user =  input('choose one, Rock, Paper, Scissors: ')
         item_user = (str(i_user)).lower()
@@ -19,29 +20,33 @@ class Game():
 
     def get_game_result(self, user_item, computer_item):
         if user_item == computer_item:
+            Game.scores['draw'] +=1
             return 'Draw'
         if user_item == 'rock' and computer_item == 'paper':
+            Game.scores['loss'] +=1
             return 'Loss'
         if user_item == 'rock' and computer_item == 'scissors':
+           Game.scores['win'] +=1   
            return 'Win'
         if user_item == 'paper' and computer_item == 'scissors':
+            Game.scores['loss'] +=1
             return 'Loss'
         if user_item == 'paper' and computer_item == 'rock':
+           Game.scores['win'] +=1
            return 'Win'
         if user_item == 'scissors' and computer_item == 'rock':
+            Game.scores['loss'] +=1
             return 'Loss'
         if user_item == 'scissors' and computer_item == 'paper':
+           Game.scores['win'] +=1
            return 'Win'
 
 
     def play(self):
-        user = yo.get_user_item()
-        pc = yo.get_computer_item()
-        result = yo.get_game_result(user, pc)
+        user = self.get_user_item()
+        pc = self.get_computer_item()
+        result = self.get_game_result(user, pc)
         print(f'You selected {user}. The computer selected {pc}. You {result}')
         return result
 
-
-
-
-
+  
--- Week5/Day5/ExercisesXP/rock-paper-scissors.py
+++ Week5/Day5/ExercisesXP/rock-paper-scissors.py
@@ -1,24 +1,28 @@
-import game
+from game import *
 
 def get_user_menu_choice():
-    selction = input('Select choice number:\n1.-Play a new game\n2.-Show scores\n3.-Quit\nchouse 1, 2 or 3: ')
-    print(selction)
+    for _ in range(1):
+        selction = input('Select choice number:\n1.-Play a new game\n2.-Show scores\n3.-Quit\nchouse 1, 2 or 3: ')
+        select = int(selction)
+        print(select)
+        return(select)
+     
 
 def print_results(results= dict):
-    print(f'The results are:\nWins: {results[list(results)[0]]} \nLoses: {results[list(results)[1]]}\nDraws: {results[list(results)[2]]}\nTks for playing')
+    print(f'The results are:\nWins: {results[list(results)[0]]} \nLoses: {results[list(results)[1]]}\nDraws: {results[list(results)[2]]}\n')
 
-def main():
-    None
+def main():    
+    get_user_menu_choice()
+    
+    if get_user_menu_choice() == 1:
+        new_game = Game()
+        new_game.play()
+        main()
+    elif get_user_menu_choice() == 2:
+        print_results(Game.scores)
+        main()
+    else:
+        print_results(Game.scores)
+        print('Tks for playing')
 
-
-Game.play()
-win = 0
-loss = 0
-draw = 0
-if yo.play() == 'win':
-    win += 1
-result_dict = {'win': 0,'loss': 0,'draw': 0}
-result_dict_keys = result_dict.keys()
-get_user_menu_choice()
-print_results(result_dict)
-print(result_dict[list(result_dict)[2]])
+main()

ec791321426cd5d54b75076b7de8e15a5ec28aab
diego ripari <diego_ripari@hotmail.com>
2023-03-17 15:54:18 +0200

    xp complete

---
+++ Week8/Day1/Xp/animals/info/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+import json
+
+# Create your views here.
+def read_data():
+    filename = 'data.json'
+
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data    
+
+def animals_list(request):
+    data = read_data()
+
+    context = {'animals': data['animals']}
+
+    return render(request, 'animals.html', context)
+
+def family_list(request):
+    data = read_data()
+
+    context = {'families': data['families']}
+    return render(request, 'families.html', context)
+
+def animal(request, id: int):
+    data = read_data()
+
+    animals_list = data['animals']
+    animal = animals_list[id-1]
+
+    context = {'animal': animal}
+
+    return render(request, 'animal.html', context)
+
+def family(request, id: int):
+    data = read_data()
+
+    family_list = data['families']
+    family = family_list[id-1]
+
+    context = {'family': family, 'animals': data['animals']}
+
+    return render(request, 'family.html', context)
